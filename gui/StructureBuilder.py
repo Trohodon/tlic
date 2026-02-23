1c133a30ab8c67ce53d4f6b378f15feb1f3152db
Trohodon <isfleshman@gmail.com>
2026-02-23 15:10:38 -0500

    Delete gui directory

--- gui/StructureBuilder.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility module mapped from StructureBuilder.cs."""
-
-from .structure_builder import StructureBuilder
-
-__all__ = ["StructureBuilder"]
--- gui/TLineVariableSweeper.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility module mapped from TLineVariableSweeper.cs."""
-
-from .tline_variable_sweeper import TLineVariableSweeper
-
-__all__ = ["TLineVariableSweeper"]
