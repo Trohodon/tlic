351146e615de6cd0bee72d65fc5ba55967dc3699
Trohodon <isfleshman@gmail.com>
2026-03-05 08:00:22 -0500

    Delete gui directory

--- gui/LineRate_MainForm.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility module mapped from LineRate_MainForm.cs."""
-
-from .line_rate_main_form import LineRateMainForm
-
-__all__ = ["LineRateMainForm"]
--- gui/MainForm.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility module mapped from MainForm.cs."""
-
-from .main_form import MainForm
-
-__all__ = ["MainForm"]
